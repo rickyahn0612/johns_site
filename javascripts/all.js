9af0235383b4f2f1b09fa0f43845f75b0a870663
rickyahn0612 <rickyahn0612@yahoo.com>
2014-07-23 09:42:57 -0700

    Site updated to e14ed6f

--- javascripts/all.js
+++ javascripts/all.js
@@ -24,8 +24,27 @@
     $('ul.main_nav li').mouseenter(function() {
       return $(this).addClass('active');
     });
-    return $('ul.main_nav li').mouseleave(function() {
+    $('ul.main_nav li').mouseleave(function() {
       return $(this).removeClass('active');
+    });
+    $('.social_media li').mouseenter(function() {
+      $(this).find('.fa-facebook').animate({
+        'color': '#4964A1'
+      }, 500);
+      $(this).find('.fa-twitter').animate({
+        'color': '#55ACEE'
+      }, 500);
+      $(this).find('.fa-instagram').animate({
+        'color': '#AA8167'
+      }, 500);
+      return $(this).find('.fa-youtube').animate({
+        'color': '#DF2826'
+      }, 500);
+    });
+    return $('.social_media li').mouseleave(function() {
+      return $(this).find('.fa').animate({
+        'color': '#fffff'
+      }, 500);
     });
   });
 
