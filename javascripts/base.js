69efea4a6c79aa2a0d7aec67d1e32194f5aab84a
rickyahn0612 <rickyahn0612@yahoo.com>
2014-07-25 11:27:31 -0700

    Site updated to ac14a54

--- javascripts/base.js
+++ javascripts/base.js
@@ -89,57 +89,6 @@
         }), "json");
       }
     });
-    $("#contact_form input, #contact_form textarea").keyup(function() {
-      $("#contact_form input, #contact_form textarea").css("border-color", "");
-      $("#result").slideUp();
-    });
-    $("#free_agent_btn").click(function() {
-      var post_data, proceed, tournament_type, user_email, user_message, user_name, user_phone;
-      tournament_type = $('select#tournament').val();
-      user_name = $("input[name=name]").val();
-      user_email = $("input[name=email]").val();
-      user_phone = $("input[name=phone]").val();
-      user_message = $("textarea[name=message]").val();
-      proceed = true;
-      if (tournament_type === "Select") {
-        $("select#tournament").css("border-color", "red");
-        proceed = false;
-      }
-      if (user_name === "") {
-        $("input[name=name]").css("border-color", "red");
-        proceed = false;
-      }
-      if (user_email === "") {
-        $("input[name=email]").css("border-color", "red");
-        proceed = false;
-      }
-      if (user_phone === "") {
-        $("input[name=phone]").css("border-color", "red");
-        proceed = false;
-      }
-      if (user_message === "") {
-        $("textarea[name=message]").css("border-color", "red");
-        proceed = false;
-      }
-      if (proceed) {
-        post_data = {
-          userName: user_name,
-          userEmail: user_email,
-          userMessage: user_message
-        };
-        $.post("free_agent_form.php", post_data, (function(response) {
-          var output;
-          if (response.type === "error") {
-            output = "<div class=\"error\">" + response.text + "</div>";
-          } else {
-            output = "<div class=\"success\">" + response.text + "</div>";
-            $("#freeagent_form input").val("");
-            $("#freeagent_form textarea").val("");
-          }
-          $("#result").hide().html(output).slideDown();
-        }), "json");
-      }
-    });
     return $("#contact_form input, #contact_form textarea").keyup(function() {
       $("#contact_form input, #contact_form textarea").css("border-color", "");
       $("#result").slideUp();
